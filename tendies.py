c81e4c6b9307e43e228bffef9f391cffece0b9d6
Adon Shapiro <adon.shapiro@tufts.edu>
2019-05-02 23:19:42 -0400

    added more foods to python version

--- tendies.py
+++ tendies.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
 
+"""
+Are there tendies in dewick? Now also featuring other foods and Carm.
+
+    --Adon Shapiro
+"""
+
 import argparse
-import os
 import re
 import requests
+import string
 import sys
 
 def parse_args(p):
     """ parses command-line arguments and returns an arguments object """
-    p.add_argument('--location', '-l', metavar='LOCATION', action='store',
-                   dest='location', choices=['dewick', 'carm'],
-                   default='dewick', help='Dewick or Carm?')
+    p.add_argument('-l', '--location', metavar='LOCATION', action='store',
+                   dest='location', choices=['dewick', 'Dewick', 'carm', 'Carm',
+                                             'carmichael', 'Carmichael'],
+                   default='dewick', help='Dewick or Carm? (Default: Dewick)')
+    p.add_argument('-i', '--item', metavar='ITEM', action='store', dest='item',
+                   default='chicken tenders',
+                   help='What are you looking for? (Default: tendies)')
     return p.parse_args()
 
 parser = argparse.ArgumentParser(description='Are there tendies in Dewick?')
 args = parse_args(parser)
-location = '11' if args.location == 'dewick' else '9'
+# Tufts' menu corresponds 11 to dewick and 9 to carm. Don't ask me why.
+location = '11' if args.location.lower() == 'dewick' else '9'
 url = 'http://menus.tufts.edu/foodpro/shortmenu.asp?locationNum=' + location
 menu = requests.get(url)
-regex = '[cC]hicken [tT]enders'
+regex = string.capwords(args.item)
 
+"""
+if menu downloaded without error, search for specified item and notify
+returns 0 on success, 1 on failure, 2 on error
+"""
 if menu.status_code != 200:
     print('Error: could not download menu', file=sys.stderr)
+    exit(2)
 elif re.search(regex, menu.text) != None:
-    os.system('notify-send "Tendies in Dewick!"')
+    print(regex + ' in ' + args.location.capitalize() + '!')
+    exit(0)
+exit(1)
