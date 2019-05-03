7ed4c2cc7b6ba45fca09a27428e95d9949a7557f
Adon Shapiro <adon.shapiro@tufts.edu>
2019-05-02 22:51:19 -0400

    added more options to python version

--- tendies.py
+++ tendies.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import re
 import requests
 import sys
 
-url = 'http://menus.tufts.edu/foodpro/shortmenu.asp?locationNum=11'
+def parse_args(p):
+    """ parses command-line arguments and returns an arguments object """
+    p.add_argument('--location', '-l', metavar='LOCATION', action='store',
+                   dest='location', choices=['dewick', 'carm'],
+                   default='dewick', help='Dewick or Carm?')
+    return p.parse_args()
+
+parser = argparse.ArgumentParser(description='Are there tendies in Dewick?')
+args = parse_args(parser)
+location = '11' if args.location == 'dewick' else '9'
+url = 'http://menus.tufts.edu/foodpro/shortmenu.asp?locationNum=' + location
 menu = requests.get(url)
+regex = '[cC]hicken [tT]enders'
 
 if menu.status_code != 200:
     print('Error: could not download menu', file=sys.stderr)
-elif re.search('[cC]hicken [tT]enders', menu.text) != None:
+elif re.search(regex, menu.text) != None:
     os.system('notify-send "Tendies in Dewick!"')
