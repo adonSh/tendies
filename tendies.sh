6328ea18bf7aab9c535162cba032cf1220d3633a
Adon Shapiro <adon.shapiro@gmail.com>
2019-08-08 08:54:52 -0500

    grep -q

--- tendies.sh
+++ tendies.sh
@@ -3,6 +3,6 @@
 url="http://menus.tufts.edu/foodpro/shortmenu.asp?locationNum=11"
 menu=`curl "$url" 2> /dev/null`
 
-if echo "$menu" | grep "Chicken Tenders" > /dev/null 2>&1; then
+if echo "$menu" | grep -q "Chicken Tenders"; then
   notify-send "Tendies in Dewick!"
 fi
