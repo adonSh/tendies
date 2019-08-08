4d062a321b2680d53525bd1f9b9c00590c17a1d5
Adon Shapiro <adon.shapiro@gmail.com>
2019-08-08 08:50:56 -0500

    lowered shell variables

--- tendies.sh
+++ tendies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-URL="http://menus.tufts.edu/foodpro/shortmenu.asp?locationNum=11"
-MENU=`curl "$URL" 2> /dev/null`
+url="http://menus.tufts.edu/foodpro/shortmenu.asp?locationNum=11"
+menu=`curl "$url" 2> /dev/null`
 
-if echo "$MENU" | grep "Chicken Tenders" > /dev/null 2>&1; then
+if echo "$menu" | grep "Chicken Tenders" > /dev/null 2>&1; then
   notify-send "Tendies in Dewick!"
 fi
